5069d4c427e90efa33a7d5d1959ad3625cbd1432
Horatiu <ilie.horatiu@yahoo.com>
2016-11-10 23:34:40 +0000

    styling added to challenge page

--- resources/views/challenge.blade.php
+++ resources/views/challenge.blade.php
@@ -4,30 +4,70 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}">
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:400,700,800" rel="stylesheet">
+
 
     </head>
 
     <body>
-        @if(Auth::user()->id == 1)
-        <a href="{{ route('challengeEnter') }}">
-            <button> Challenge User 2 </button>
+        <a style="float:right; margin-right:1em;" href="{{ route('logout') }}">
+            <button id="button" class="blue globalRadius"> Logout </button>
         </a>
-        @else
-        <a href="{{ route('challengeEnter') }}">
-            <button> Challenge User 1 </button>
-        </a>
-        @endif
-        <a href="{{ route('logout') }}">
-            <button> Logout </button>
-        </a>
+        <div id="content">
+            <div id="content-2x" style="top: 150px;">
+                <center>
+                    <h4 style="color:#000;">GameBet Demo - Challenge Page</h4>
+                    <h1 style="color:#000;">Please select a user to challenge</h1>
+                    @if(Auth::user()->id == 1)
+                      <button id="button" class="blue globalRadius" type="button">Login as User 1</button>
+                    @else
+                      <button id="button" class="blue globalRadius" type="button">Login as User 2</button>
+                    @endif
+                </center>
+            </div>
+        </div>
 
-        @if(Auth::check())
-            Logged in as {{Auth::user()->name}}
-        @endif
     </body>
 </html>
+
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous">
+</script>
+
+<script>
+    $('.blue').click(function(){
+        $.ajax({
+            method:'POST',
+            url: '{{ route("login") }}',
+            data:{id:1},
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success:function(data){
+                window.location.href = "{{ route('challengePage') }}";
+            }
+        });
+    });
+
+    $('.green').click(function(){
+        $.ajax({
+            method:'POST',
+            url: '{{ route("login") }}',
+            data:{id:2},
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success:function(data){
+                window.location.href = "{{ route('challengePage') }}";
+            }
+        });
+    });
+</script>
