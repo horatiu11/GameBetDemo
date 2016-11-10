e85c5ac10b6651d4515008407aad25b361deb35e
Horatiu <ilie.horatiu@yahoo.com>
2016-11-10 23:36:57 +0000

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>GameBet Demo</title>
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}">
         <!-- Fonts -->
