b301715003982cdf0ea5145593f12a6045851052
Horatiu <ilie.horatiu@yahoo.com>
2016-11-10 22:24:08 +0000

    Created challenges db and controller

---
+++ app/Http/Controllers/ChallengeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Input;
+use App\User;
+use App\Challenge;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+class ChallengeController extends Controller
+{
+    public function joinChallenge(Request $request)
+    {
+        //Updating an existing challenge or creating a new one
+        $user = Auth::user();
+
+        $challenge = Challenge::where('user1_id', '=', $user->id)->first();
+
+        if($challenge == null){
+            $challenge = Challenge::where('user2_id', '=', $user->id)->first();
+        }
+
+        if($challenge == null){
+            $challenge = Challenge::where('state', '=', 1)->first();
+            if($challenge == null){
+                $challenge = new Challenge();
+                $challenge->user1_id = $user->id;
+                $challenge->state = 1;
+                $challenge->save();
+            } else{
+                if($challenge->user2_id == null){
+                    $challenge->user2_id = $user->id;
+                    $challenge->state = 2;
+                    $challenge->save();
+                } else{
+                    $challenge->user1_id = $user->id;
+                    $challenge->state = 2;
+                    $challenge->save();
+                }
+            }
+            return redirect()->route('/');
+        } else{
+            return redirect()->back();
+        }
+    }
+}
---
+++ database/migrations/2016_11_10_212721_create_challenges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user1_id')->nullable();
+            $table->integer('user2_id')->nullable();
+            $table->integer('state')->default(0);
+            $table->timestamps();
+            $table->timestamp('ended_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
