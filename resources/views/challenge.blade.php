9b9fcf789aa44222e9a805a93162b29b0643772a
Horatiu <ilie.horatiu@yahoo.com>
2016-11-10 22:47:57 +0000

    Added challenge page

---
+++ resources/views/challenge.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+        @if(Auth::user()->id == 1)
+        <a href="/login/1">
+            <button> Challenge User 2 </button>
+        </a>
+        @else
+        <a href="/login/2">
+            <button> Challenge User 1 </button>
+        </a>
+        @endif
+        <a href="/logout">
+            <button> Logout </button>
+        </a>
+
+        @if(Auth::check())
+            Logged in as {{Auth::user()->name}}
+        @endif
+    </body>
+</html>
